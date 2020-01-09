57aad41500d32b29b5bec6e11555c628a67057d7
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-09 01:56:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     @@all
   end
   
-  def new_by_filename(fileName)
+  def self.new_by_filename(fileName)
     artist, song = file.split(" - ")
     newSong = Song.new(song)
     newSong.artist_name = artist
